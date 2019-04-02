00821543c2644f03a5d5fe07b169adf96ece19cc
NatsSsor <b5021365@my.shu.ac.uk>
2019-04-02 22:35:31 +0100

    prep for messenger and cors bypass
    for shane

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 
 //("mongodb://den1.mongo1.gear.host:27001/pibotdb");
-
+app.options("/*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.sendStatus(200);
+});
 //"mongodb://pibotdb:password1!@den1.mongo1.gear.host:27001/pibotdb"
 const connstring = "mongodb://den1.mongo1.gear.host:27001/pibotdb";
 const connstring2 =
---
+++ routes/MessengerRoutes.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+//page access token 410062269784654
+router.post("/getPageAccessToken", function(req, res, next) {});
