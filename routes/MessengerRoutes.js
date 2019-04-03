ecaa31e86e0f4c4ec3abe10c26d8971f2b85c276
NatsSsor <b5021365@my.shu.ac.uk>
2019-04-03 09:26:16 +0100

    Update MessengerRoutes.js

--- routes/MessengerRoutes.js
+++ routes/MessengerRoutes.js
@@ -5,9 +5,4 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var passport = require("passport");
 
-//page access token 410062269784654
-router.get("/getPageAccessToken", function(req, res, next) {
-  passport.authenticate("facebook", { scope: ["manage_pages"] });
-});
-
 module.exports = router;
